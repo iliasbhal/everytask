05575d260c31fe6aea475ad70831b0300614a40b
iliasbhal <ilias.bhal@gmail.com>
2021-02-23 18:42:19 -0500

    tests: added tests for browser environement

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,6 @@
 import * as tasks from '../src';
+import * as path from 'path';
+import playwright from 'playwright';
 
 
 describe('EveryTask', () => {
@@ -22,6 +24,58 @@ describe('EveryTask', () => {
     expect(callOrder).toMatchObject([0,0,1,1,2,2,3,3]);
 
   });
+
+  for (const browserType of ['chromium', 'firefox', 'webkit'] as const) {
+    it(`should call callback in correct order in ${browserType}`, async () => {
+  
+      const browser = await playwright[browserType].launch();
+      const page = await browser.newPage();
+      await page.setContent(`
+        <html>
+          <head>
+            <meta charset="UTF-8">
+          </head>
+          <body>
+            <script>
+              var exports = {};
+            </script>
+          </body>
+        </html>
+      `);
+  
+      await page.addScriptTag({
+        path: path.resolve(__dirname, "../dist/everytask.cjs.production.min.js"),
+      });
+  
+      await page.addScriptTag({
+        content: `
+          const calls = [];
+          const tasks = exports;
+
+          tasks.task(() => calls.push(3));
+          tasks.task(() => calls.push(3));
+          tasks.macrotask(() => calls.push(2));
+          tasks.macrotask(() => calls.push(2));
+          tasks.microtask(() => calls.push(1));
+          tasks.microtask(() => calls.push(1));
+          tasks.synchronous(() => calls.push(0));
+          tasks.synchronous(() => calls.push(0));
+        `
+      });
+
+      await wait(400);
+  
+      const callOrder = await page.evaluate(() => {
+        // @ts-ignore;
+        return calls;
+      });
+  
+      expect(callOrder).toMatchObject([0,0,1,1,2,2,3,3]);
+  
+      await browser.close();
+    })
+  }
+
 
   it('should handle fuzzing test in node.js', async () => {
     const spy = jest.fn();
@@ -52,5 +106,67 @@ describe('EveryTask', () => {
     })
     expect(isCalledInRightOrder).toBe(true);
   });
+
+  for (const browserType of ['chromium', 'firefox', 'webkit'] as const) {
+    it(`should handle fuzzing test in ${browserType}`, async () => {
+      const browser = await playwright[browserType].launch();
+      const page = await browser.newPage();
+      await page.setContent(`
+        <html>
+          <head>
+            <meta charset="UTF-8">
+          </head>
+          <body>
+            <script>
+              var exports = {};
+            </script>
+          </body>
+        </html>
+      `);
+  
+      await page.addScriptTag({
+        path: path.resolve(__dirname, "../dist/everytask.cjs.production.min.js"),
+      });
+  
+      await page.addScriptTag({
+        content: `
+          const calls = [];
+          const tasks = exports;
+
+          const taskNameByPriority = {
+            task: 3,
+            macrotask: 2,
+            microtask: 1,
+            synchronous: 0,
+          };
+      
+          const taskNames = Object.keys(tasks);
+          const taskCreators = Array.from({ length: 4000 })
+            .map((_) => {
+              var randomTaskName = taskNames[Math.floor(Math.random() * taskNames.length)];
+              const taskCreator = tasks[randomTaskName];
+              const priority = taskNameByPriority[randomTaskName];
+              return () => taskCreator(() => calls.push(priority))
+            });
+      
+          taskCreators.forEach((taskCreator) => taskCreator());
+        `
+      });
+
+      await wait(400);
+  
+      const callOrder : number[] = await page.evaluate(() => {
+        // @ts-ignore;
+        return calls;
+      });
+  
+      const isCalledInRightOrder = callOrder.every((priority, i, arr) => {
+        return i === 0 || priority >= arr[i-1];
+      })
+      expect(isCalledInRightOrder).toBe(true);
+
+      await browser.close();
+    })
+  }
 });
 
